24d31c9e146a89721e10ab4b1fd30d2fe0b8cf1a
siddharthgaglani <siddharthgaglani@gmail.com>
2020-11-20 23:36:59 +0530

    initial commit

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+
+// axios.create({
+//   baseUrl: 'https://api.unsplash.com',
+//   headers: {
+//     Authorization: 'Client-ID 1b5iiIj9X0zPzxwa32Vq57JkpYdH4shfMylsTdQTd7E'
+//   }
+// })
---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+
+
+class App extends React.Component {
+
+  state = { images:[] };
+
+  onSearchSubmit = async (term)=>{
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID 1b5iiIj9X0zPzxwa32Vq57JkpYdH4shfMylsTdQTd7E'
+      }
+    });
+    // using promise
+    // .then((response) => {
+    //   console.log(response.data.results);
+    // });
+
+    this.setState({ images:response.data.results });
+
+  }
+
+  render(){
+    return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar onSubmit={this.onSearchSubmit} />
+      <ImageList images={this.state.images }/>
+    </div>
+    );    
+  }
+}
+
+export default App;
